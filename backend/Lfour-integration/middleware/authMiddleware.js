7884a6ea15ad25a73f9a52e91452bbb52cae088f
olaniyi zainab <olaniyizainab876@gmail.com>
2026-06-29 12:03:26 +0100

    real

--- backend/Lfour-integration/middleware/authMiddleware.js
+++ backend/Lfour-integration/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 const User=require("../models/userModel") 
 const jwt = require("jsonwebtoken");
-const asyncHandler = require('express-async-Handler');
+const asyncHandler = require('express-async-handler');
 
 
 const adminProtect=asyncHandler(async (req,res,next)=>{
