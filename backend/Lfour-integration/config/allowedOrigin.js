1785d359524cf21c656b6620e02e15ad309ba5a0
olaniyi zainab <olaniyizainab876@gmail.com>
2026-06-30 14:47:25 +0100

    Fix FeaturedProducts import

--- backend/Lfour-integration/config/allowedOrigin.js
+++ backend/Lfour-integration/config/allowedOrigin.js
@@ -1,7 +1,9 @@
+const frontend_url=process.env.CLIENT_URL 
 const allowedOrigin = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  frontend_url
 ];
 
 module.exports=allowedOrigin
